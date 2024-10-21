87bf93615012e2572dcedad8426e5b62a4db04c4
Juan Miguel <jmhernan@yahoo.com>
2024-10-20 22:59:37 -0600

    Configurando Settings.py, DEBUG

--- GeProScrum/settings.py
+++ GeProScrum/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#96g7n=k%!if-)0d4fwd(#uk5oa7hj#vpo&a#%!9+h28_pc+#i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 #ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', 'tu-dominio.com']
